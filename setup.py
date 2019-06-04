d363968a25b147612a5c359cb6cdacb9d8f1a1cc
dallonsi <damien.allonsius@univ-amu.fr>
2019-06-04 16:28:09 +0200

    configured the setup.py file

--- setup.py
+++ setup.py
@@ -4,14 +4,17 @@ packages_ = find_packages()
 packages = [p for p in packages_ if not(p == 'tests')]
 
 setup(name='agents_option',
-      version='0',
+      version='0.T',
       description='A package to create agent following a Hierarchical Reinforcement Learning strategy',
       url='git@github.com:DamienAllonsius/agent_option_library.git',
       author='Damien Allonsius',
       author_email='allonsius.damien@hotmail.fr',
       license='MIT',
       packages=packages,
-      install_requires=[
-        'python-dateutil', 'docopt==0.6.2', 'numpy==1.15.4', 'scipy==1.1.0', 'sphinx', 'gym'
-      ],
+      install_requires=['numpy==1.15.4',
+                        'setuptools==39.0.1',
+                        'docopt==0.6.2',
+                        'tqdm==4.29.0',
+                        'python_dateutil==2.8.0',
+                        'testresources==2.0.1 '],
       zip_safe=False)
