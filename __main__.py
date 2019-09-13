faf335d5fdb5a9202ddfa2e47acfa601b59fc4dc
dallonsi <damien.allonsius@univ-amu.fr>
2019-09-13 14:56:05 +0200

    added the observation_wrapper and cleaned a bit: file structures/graph and policies/policy_option have been deleted

--- __main__.py
+++ __main__.py
@@ -2,7 +2,8 @@ import sys
 
 # only unit testing for the moment
 if __name__ == "__main__":
-    from tests.test_policy_manager import *
+    # import your tests like
+    # from tests.test_policy_manager import *
     import unittest
 
     del sys.argv[1:]
